dad3518abe1ea894e977d333a0ac1c223ee545e0
Viktor Hasle <vikhasle@icloud.com>
2026-05-04 13:55:08 +0200

    Testing build

--- src/App.tsx
+++ src/App.tsx
@@ -10,33 +10,33 @@ import TeamDebrief from './screens/TeamDebrief';
 import type { ScreenId } from './types';
 
 function AppInner() {
-  const { role } = useRole();
-  const [activeScreen, setActiveScreen] = useState<ScreenId>('backbone');
+	const { role } = useRole();
+	const [activeScreen, setActiveScreen] = useState<ScreenId>('backbone');
 
-  function navigate(s: ScreenId) {
-    if (role.screens.includes(s)) setActiveScreen(s);
-  }
+	function navigate(s: ScreenId) {
+		if (role.screens.includes(s)) setActiveScreen(s);
+	}
 
-  return (
-    <>
-      <ProtoBar activeScreen={activeScreen} onNavigate={navigate} />
-      <div className="screen-wrap">
-        {activeScreen === 'backbone' && <DayBackbone activeScreen={activeScreen} onNavigate={navigate} />}
-        {activeScreen === 'capture'  && <Capture activeScreen={activeScreen} onNavigate={navigate} />}
-        {activeScreen === 'intel'    && <Intelligence activeScreen={activeScreen} onNavigate={navigate} />}
-        {activeScreen === 'debrief'  && <TeamDebrief activeScreen={activeScreen} onNavigate={navigate} />}
-      </div>
-      <BottomNav activeScreen={activeScreen} onNavigate={navigate} />
-    </>
-  );
+	return (
+		<>
+		<ProtoBar activeScreen={activeScreen} onNavigate={navigate} />
+		<div className="screen-wrap">
+		{activeScreen === 'backbone' && <DayBackbone activeScreen={activeScreen} onNavigate={navigate} />}
+		{activeScreen === 'capture'  && <Capture activeScreen={activeScreen} onNavigate={navigate} />}
+		{activeScreen === 'intel'    && <Intelligence activeScreen={activeScreen} onNavigate={navigate} />}
+		{activeScreen === 'debrief'  && <TeamDebrief activeScreen={activeScreen} onNavigate={navigate} />}
+		</div>
+		<BottomNav activeScreen={activeScreen} onNavigate={navigate} />
+		</>
+	);
 }
 
 export default function App() {
-  return (
-    <ThemeProvider>
-      <RoleProvider>
-        <AppInner />
-      </RoleProvider>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider>
+		<RoleProvider>
+		<AppInner />
+		</RoleProvider>
+		</ThemeProvider>
+	);
 }
