ad5e83a935b66d7191996babd704d3da3a8f6a11
Viktor Hasle <vikhasle@icloud.com>
2026-05-19 12:00:38 +0200

    Fixing import issues

--- src/audioStream.ts
+++ src/audioStream.ts
@@ -1,5 +1,48 @@
-import workletUrl from '@/audio-processor.js?url'
 import { MicVAD } from "@ricky0123/vad-web";
+
+class AudioProcessor extends AudioWorkletProcessor {
+	constructor() {
+		super();
+
+		this.buffer = [];
+		this.bufferSize = 4096;
+	}
+
+	process(inputs) {
+		const input = inputs[0];
+
+		if (input.length > 0) {
+			const channel = input[0];
+
+			for (const element of channel) {
+				const s = Math.max(-1, Math.min(1, element));
+
+				const sample
+				= s < 0
+					? s * 0x80_00
+					: s * 0x7F_FF;
+
+					this.buffer.push(sample);
+			}
+
+			if (this.buffer.length >= this.bufferSize) {
+				const pcm16
+				= new Int16Array(this.buffer);
+
+				this.port.postMessage(pcm16);
+
+				this.buffer = [];
+			}
+		}
+
+		return true;
+	}
+}
+
+registerProcessor(
+	'audio-processor',
+	AudioProcessor,
+);
 
 export default async function startAudioStreaming() {
 
