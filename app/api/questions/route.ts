2617aa3fc4ff071810198cf77e2a2f688661b55b
Viktor Hasle <vikhasle@icloud.com>
2026-08-06 15:02:16 +0200

    Questions Api

--- app/api/questions/route.ts
+++ app/api/questions/route.ts
@@ -1,5 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+/*
+ * Handle Questions set with own route, or many calls to Questions?
+ */
+
 const BASE = 'https://wriggly-tutu-groin.ngrok-free.dev';
 const HEADERS = { 'ngrok-skip-browser-warning': '1' };
 
@@ -29,4 +33,23 @@ export async function GET(req: NextRequest) {
  const res = await upstream(path);
  const questions = await res.json();
  return NextResponse.json(questions);
+}
+
+// PATCH /api/questions?id=int1
+export async function PATCH(req: NextRequest){
+ const {searchParams} = new URL(req.url);
+ const question_id = searchParams.get('id');
+ const path = '/questions/${id}'
+
+ const res = await upstream(path);
+ return NextResponse.json(res);
+}
+// DELETE /api/questions?id=int1
+export async funtion DELETE(req: NextRequest){
+ const {searchParams} = new URL(req.url);
+ const question_id = searchParams.get('id');
+ const path = '/questions/${id}'
+
+ const res = await upstream(path);
+ return NextResponse.json(res);
 }
